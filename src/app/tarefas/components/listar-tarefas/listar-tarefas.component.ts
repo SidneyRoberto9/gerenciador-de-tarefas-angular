096b0d522e23e144dcb188f336f4c8d94ab3ed36
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-21 11:05:26 -0300

    ✨ feat: add functions and populate html listar tarefas

--- src/app/tarefas/components/listar-tarefas/listar-tarefas.component.ts
+++ src/app/tarefas/components/listar-tarefas/listar-tarefas.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarefa, TarefaService } from '../../shared';
 
 @Component({
   selector: 'app-listar-tarefas',
   templateUrl: './listar-tarefas.component.html',
-  styleUrls: ['./listar-tarefas.component.css']
+  styleUrls: ['./listar-tarefas.component.css'],
 })
 export class ListarTarefasComponent implements OnInit {
+  tarefas: Tarefa[];
 
-  constructor() { }
+  constructor(private tarefaService: TarefaService) {}
 
   ngOnInit(): void {
+    this.tarefas = this.listarTodos();
   }
 
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
 }
