b30c0d236902262e4986bc59d71e2e2613d38b0b
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-21 11:05:02 -0300

    ✨ feat: add router and forms module to tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './shared';
 import { ListarTarefasComponent } from './components';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ListarTarefasComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   providers: [TarefaService],
 })
 export class TarefasModule {}
