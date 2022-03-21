ce07612d6901969ec232473cc766e32ff7ad078d
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-21 13:28:35 -0300

    ✨ feat: use directive

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TarefaService } from './shared';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import {
   ListarTarefasComponent,
   CadastrarTarefaComponent,
   EditarTarefaComponent,
 } from './components';
-import { TarefaConcluidaDirective } from './shared/tarefa-concluida.directive';
 
 @NgModule({
   declarations: [
