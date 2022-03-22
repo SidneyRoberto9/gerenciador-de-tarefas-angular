1343c7d39ed4117accbe6b9b13ae183179b33a90
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-22 12:03:41 -0300

    ✨ feat: create basic theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Gerenciador de Tarefas';
+  theme: Theme = 'white-theme';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeBg();
+  }
+
+  initializeBg() {
+    this.renderer.addClass(this.document.body, this.theme);
+  }
 }
+
+export type Theme = 'white-theme';
